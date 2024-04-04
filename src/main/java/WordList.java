1e891b799a240daa2d8cb51213136f6a0c0ac05d
hyunjaeoh7 <143122764+hyunjaeoh7@users.noreply.github.com>
2024-04-04 11:47:03 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -10,13 +10,24 @@ public class WordList
 
   public int numWordsOfLength(int len)
   {
-    /* to be implemented in part (a) */
-    
+    int count = 0;
+    for(int i = 0; i < myList.length; i++){
+      if(myList[i].length() == len){
+        count++;
+      }
+    }
+    return count;
   }
 
   public void removeWordsOfLength(int len)
   {
-    /* to be implemented in part (b) */
-
-  }
+    int i = 0;
+    int co = 0;
+    while(i < myList.length){
+      if(myList[i-co].length() == len){
+        myList.remove(i-co);
+        co++;
+      }
+      i++;
+    }
 }
